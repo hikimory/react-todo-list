92838097fffb25dbdb12fdd73fa5cad631557d3c
Nick Bykon <nikita.bikon@gmail.com>
2020-07-29 14:59:18 +0300

    add state for checkbox

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React from 'react';
 import TodoList from './Todo/TodoList'
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     {id: 1, completed: false, title: 'Buy bread'},
     {id: 2, completed: false, title: 'Buy oil'},
     {id: 3, completed: false, title: 'Buy milk'},
-  ]
+  ])
+
+  function toggleTodo(id){
+    setTodos (todos.map(todo => {
+        if(todo.id === id)
+        {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
 
   return (
     <div className="wrapper">
       <h1>React tutorial</h1>
-      < TodoList todos={todos} />
+      < TodoList todos={todos} onToggle={toggleTodo}/>
     </div>
   )
 }
