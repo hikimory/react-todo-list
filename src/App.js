62005129315ab325367442c74aa6cc67f9642d1a
Nick Bykon <nikita.bikon@gmail.com>
2020-07-29 16:57:43 +0300

    add download from server and loader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TodoList from './Todo/TodoList'
 import Context from './context'
 import AddTodo from './Todo/AddTodo'
+import Loader from './Loader'
 
 function App() {
-  const [todos, setTodos] = React.useState([
-    {id: 1, completed: false, title: 'Buy bread'},
-    {id: 2, completed: false, title: 'Buy oil'},
-    {id: 3, completed: false, title: 'Buy milk'},
-  ])
+  const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(response => response.json())
+    .then(todos => {
+      setTimeout(() => {
+        setTodos(todos)
+        setLoading(false)
+      }, 2000)
+    })
+  }, [])
 
   function toggleTodo(id){
     setTodos (todos.map(todo => {
@@ -32,13 +41,19 @@ function App() {
   function removeTodo(id){
     setTodos(todos.filter(todo => todo.id !== id))
   }
- // {removeTodo: removeTodo} = {removeTodo}
+
+
   return (
     <Context.Provider value={{removeTodo, toggleTodo}}> 
       <div className="wrapper">
         <h1>React tutorial</h1>
         <AddTodo onCreate={addTodo}/>
-        {todos.length ? < TodoList todos={todos} /> : <p>No todos !</p>}
+
+        { loading && <Loader />}
+        {todos.length ? (
+          < TodoList todos={todos} />
+        ) : (
+        loading ? null : <p>No todos !</p>)}
       </div>
     </Context.Provider>
   )
